d5bd01bce4cdba28bc5bef0050ae4b802e4a0621
Greg <greigdillon@gmail.com>
2019-10-30 09:37:14 -0500

    Added Mageplaza Slider Table

--- app/code/TheDeveloper/Banner/Setup/InstallSchema.php
+++ app/code/TheDeveloper/Banner/Setup/InstallSchema.php
@@ -152,6 +152,67 @@ class InstallSchema implements InstallSchemaInterface{
                 ['name','image','url_banner'],
                 AdapterInterface::INDEX_TYPE_FULLTEXT
             );
+
+            if(!$installer->tableExists('mageplaza_bannerslider')){
+                $table = $installer->getConnection()
+                    ->newTable($installer->getTable('mageplaza_bannerslider_slider'))
+                    ->addColumn(
+                        'slider_id',
+                        Table::TYPE_INTEGER,
+                        null,
+                        [
+                            'identity' => true,
+                            'nullable' => false,
+                            'primary' => true,
+                            'unsigned' => true
+                        ],
+                        'Slider ID'
+                    )
+                    ->addColumn('name',Table::TYPE_TEXT,255,['nullable' => 'false'],'Slider Name')
+                    ->addColumn('status', Table::TYPE_SMALLINT,null,['nullable' => false,'default' => '1'],'Status')
+                    ->addColumn('location',Table::TYPE_TEXT,1000,[],'Location')
+                    ->addColumn('store_ids',Table::TYPE_TEXT,255,[])
+                    ->addColumn('customer_group_ids',Table::TYPE_TEXT,255)
+                    ->addColumn('priority',Table::TYPE_INTEGER,null,[
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'default' => '0'
+                    ],
+                        'Priority')
+                    ->addColumn('effect',Table::TYPE_TEXT,255,[],'Animation effect')
+                    ->addColumn('autoWidth',Table::TYPE_SMALLINT,null,[],'Auto Width')
+                    ->addColumn('autoHeight',Table::TYPE_SMALLINT,null,[],'Auto Height')
+                    ->addColumn('design',Table::TYPE_SMALLINT,null,[
+                        'nullable' => false,
+                        'default' => '0'
+                    ],'Design')
+                    ->addColumn('loop',Table::TYPE_SMALLINT,null,'Loop Slider')
+                    ->addColumn('lazyload',Table::TYPE_SMALLINT,null,'Lazyload Image')
+                    ->addColumn('autoplay',Table::TYPE_SMALLINT,null,[],'Autoplay')
+                    ->addColumn('autoplayTimeout',Table::TYPE_TEXT,255,[
+                        'default' => '5000'
+                    ],'Autoplay Timeout')
+                    ->addColumn('nav',Table::TYPE_SMALLINT,null,[],'Navigation')
+                    ->addColumn('dots',Table::TYPE_SMALLINT,null,[],'Dots')
+                    ->addColumn('is_responsive',Table::TYPE_SMALLINT,null,'Responsive')
+                    ->addColumn('responsive_items',Table::TYPE_TEXT,null,[],'Max Items Slider')
+                    ->addColumn('from_date',Table::TYPE_DATE,null,[
+                        'nullable' => true,
+                        'default' => null
+                    ],'From')
+                    ->addColumn('to_date',Table::TYPE_DATE,[
+                        'nullable' => true,
+                        'default' => null
+                    ])
+                    ->addColumn('created_at',Table::TYPE_TIMESTAMP,null,[],'Slider Created At')
+                    ->addColumn('updated_at',Table::TYPE_TIMESTAMP,null,[],'Slider Updated At')
+                    ->setComment('Slider Table');
+
+                $installer->getConnection()->createTable($table);
+
+            }
+
+
         }
     }
 }
